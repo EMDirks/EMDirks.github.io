4c3918d5c50fb1dfe22f82f38855b8f8206a2451
Ethan Dirks <ethandirks94@gmail.com>
2020-04-06 14:39:41 -0700

    Add files via upload

---
+++ ab/js/script.js
@@ -0,0 +1,48 @@
+var isGameFullScreen = false;
+
+// ---------------------------- //
+// ------- FULL SCREEN -------- //
+// ---------------------------- //
+
+$(".full-screen").click(function(){
+	if (isGameFullScreen === false) {
+		openFullscreen();
+		isGameFullScreen = true;
+		return false;
+	}
+	if (isGameFullScreen === true) {
+		closeFullscreen();
+		isGameFullScreen = false;
+		return false;
+	}
+});
+
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
