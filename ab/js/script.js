3e664d425fa24ad300c489f2767ef89d5e0a15c2
Ethan Dirks <ethandirks94@gmail.com>
2020-04-06 16:16:37 -0700

    Add files via upload

--- ab/js/script.js
+++ ab/js/script.js
@@ -1,48 +1,16 @@
-var isGameFullScreen = false;
+var butt = document.getElementById("full-screen");
 
-// ---------------------------- //
-// ------- FULL SCREEN -------- //
-// ---------------------------- //
-
-$(".full-screen").click(function(){
-	if (isGameFullScreen === false) {
-		openFullscreen();
-		isGameFullScreen = true;
-		return false;
-	}
-	if (isGameFullScreen === true) {
-		closeFullscreen();
-		isGameFullScreen = false;
-		return false;
-	}
+butt.addEventListener("click", function() {
+  toggleFullScreen();
 });
 
 
-/* Get the documentElement (<html>) to display the page in fullscreen */
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
-/* Close fullscreen */
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) { /* Firefox */
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
-    document.msExitFullscreen();
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); 
+    }
   }
 }
